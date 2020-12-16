59fdc752f2581c8d9f05d9deba9018c5fb03c88c
gan3i <gani468@gmail.com>
2020-12-16 19:46:26 +0530

    is deciaml number a palindrome

--- EPIJudge-master/epi_judge_python/is_number_palindromic.py
+++ EPIJudge-master/epi_judge_python/is_number_palindromic.py
@@ -3,6 +3,29 @@ from test_framework import generic_test
 
 def is_palindrome_number(x: int) -> bool:
     # TODO - you fill in here.
+
+    '''
+    x = 313
+    j = 2
+    l = 3
+    r = 3
+    '''
+    j =0
+    temp = x
+    while temp >= 10:
+        temp = temp // 10
+        j +=1
+
+    while x:
+        l = x % 10
+        r = x // (10**j)
+        if l != r:
+            return False
+        x = (x - (10 ** j * r)) // 10
+
+        j -=2 
+
+
     return True
 
 
