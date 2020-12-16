25396ee722c33fcae9b4aaaeab0fee84e0dd43ab
gan3i <gani468@gmail.com>
2020-12-16 09:16:37 +0530

    using precomputed parity to calculate parity

--- EPIJudge-master/epi_judge_python/parity.py
+++ EPIJudge-master/epi_judge_python/parity.py
@@ -1,13 +1,45 @@
 from test_framework import generic_test
+from typing import List
+from precomputed_parity import PRECOMPUTED_PARITY
+
+#without cache, by errasing the least set bit everytime O(k) where k is the number of the set bit
+# def parity(x : int) ->int:
+
+#     result = 0 
+#     while x:
+#         result ^=1
+#         x &=x-1
+#     return result
 
 
+#precompute the parity of all first 2** 16 integers
+# def precompute(precomputed_parity: List[int]) -> None:
+
+#     y = 0
+#     while y < 2 ** 16:
+#         precomputed_parity[y] = parity(y)
+#         y += 1
+#     file = open("precomputed_parity.txt","wt")
+#     file.write('[' + ','.join(str(x) for x in precomputed_parity) + ']')
+
+
+
+#Compute the parity by using the precomputed parity, bit masking and bit fiddling
 def parity(x: int) -> int:
     # TODO - you fill in here.
-    result = 0
-    while x:
-        result ^= 1
-        x &=(x-1)
-    return result
+    # precomputed_parity = [0 for _ in range((2**16))]
+    # precompute(precomputed_parity)
+
+    mask_size = 16
+    bit_mask = 2 ** 16 - 1 # or 0xFFFF
+
+    return PRECOMPUTED_PARITY[x >> (3 * mask_size)] ^ \
+    PRECOMPUTED_PARITY[(x >> (2 * mask_size)) & bit_mask] ^ \
+    PRECOMPUTED_PARITY[(x >> (mask_size)) & bit_mask] ^ \
+    PRECOMPUTED_PARITY[x & bit_mask]
+
+# print(parity(14))
+
 
 
 if __name__ == '__main__':
