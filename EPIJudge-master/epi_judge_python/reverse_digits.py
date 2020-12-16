072f36b47ba01a01ba6441b903cb268c5e1b637a
gan3i <gani468@gmail.com>
2020-12-16 19:17:45 +0530

    reverse digit

--- EPIJudge-master/epi_judge_python/reverse_digits.py
+++ EPIJudge-master/epi_judge_python/reverse_digits.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def reverse(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    sign = 1
+    if x < 0:
+        sign = -1
+        x *=-1
+
+    result = 0
+
+    while x :
+        mod = x % 10
+        result = (result * 10) + mod
+        x =x//10
+
+    return result * sign
 
 
 if __name__ == '__main__':
