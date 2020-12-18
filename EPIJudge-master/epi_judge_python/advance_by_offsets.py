15db2c01852077414580bcc87f2cdd869b9b9e8a
gan3i <gani468@gmail.com>
2020-12-18 12:03:28 +0530

    advance through the array

--- EPIJudge-master/epi_judge_python/advance_by_offsets.py
+++ EPIJudge-master/epi_judge_python/advance_by_offsets.py
@@ -5,6 +5,18 @@ from test_framework import generic_test
 
 def can_reach_end(A: List[int]) -> bool:
     # TODO - you fill in here.
+    furth_most  = 0
+    i = 0
+    last_index = len(A) - 1
+
+
+    while i <= furth_most & furth_most < last_index:
+        furth_most = max(furth_most, A[i] + i)
+        i += 1
+
+    if furth_most < last_index:
+        return False   
+
     return True
 
 
