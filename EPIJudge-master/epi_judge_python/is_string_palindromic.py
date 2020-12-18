41654673025c4f57d7518d264cbf9d46055a3859
gan3i <gani468@gmail.com>
2020-12-18 12:57:51 +0530

    palindrome string

--- EPIJudge-master/epi_judge_python/is_string_palindromic.py
+++ EPIJudge-master/epi_judge_python/is_string_palindromic.py
@@ -3,6 +3,11 @@ from test_framework import generic_test
 
 def is_palindromic(s: str) -> bool:
     # TODO - you fill in here.
+
+    for i in range(len(s)//2):
+        if s[i].lower() != s[len(s)-1-i].lower():
+            return False
+
     return True
 
 
