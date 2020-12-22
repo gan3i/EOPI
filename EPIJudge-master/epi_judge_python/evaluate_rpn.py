47163ee2a2929fd2600280f841d8c25cdcc87047
gan3i <gani468@gmail.com>
2020-12-22 23:26:26 +0530

    reverse polish number

--- EPIJudge-master/epi_judge_python/evaluate_rpn.py
+++ EPIJudge-master/epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
-
+from queue import LifoQueue
 
 def evaluate(expression: str) -> int:
     # TODO - you fill in here.
-    return 0
+    intermediate_results = LifoQueue()
+
+    operators = {'+':lambda x, y: x + y, 
+                '-' : lambda x, y: y - x,
+                '*' : lambda x, y: x * y,
+                '/' : lambda x, y: y // x}
+    for value in expression.split(','):
+        if value in operators:
+                intermediate_results.put(operators[value](intermediate_results.get() , intermediate_results.get()))
+        else:
+            intermediate_results.put(int(value))  
+
+    return intermediate_results.get()
 
 
 if __name__ == '__main__':
