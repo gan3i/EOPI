93d326e1b4b2d2be1235f5bb938194520cc758d3
gan3i <gani468@gmail.com>
2020-12-23 23:23:07 +0530

    level order traversal

--- EPIJudge-master/epi_judge_python/tree_level_order.py
+++ EPIJudge-master/epi_judge_python/tree_level_order.py
@@ -3,10 +3,21 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
-
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    result : List[List[int]] = []
+    if not tree:
+        return result
+
+    current_depth_nodes = [tree]
+
+    while current_depth_nodes:
+        result.append([node.data for node in current_depth_nodes if node])
+
+        current_depth_nodes = [child for curr in current_depth_nodes for child in (curr.left, 
+                            curr.right) if child]
+
+    return result
 
 
 if __name__ == '__main__':
