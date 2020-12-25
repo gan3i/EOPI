a2dbcf2a474754317323ea718f30c8fed5efc923
gan3i <gani468@gmail.com>
2020-12-25 16:18:22 +0530

    check symmetry of the tree

--- EPIJudge-master/epi_judge_python/is_tree_symmetric.py
+++ EPIJudge-master/epi_judge_python/is_tree_symmetric.py
@@ -4,7 +4,18 @@ from test_framework import generic_test
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
     # TODO - you fill in here.
-    return True
+    def check_symmetry(subtree_1, subtree_2):
+        if not subtree_1 and not subtree_2:
+            return True
+        if subtree_1 and subtree_2:
+            return (subtree_1.data == subtree_2.data 
+                    and check_symmetry(subtree_1.left, subtree_2.right) 
+                    and check_symmetry(subtree_2.left, subtree_1.right))
+        return False
+    if not tree:
+        return True
+    else:
+        return check_symmetry(tree.left, tree.right)
 
 
 if __name__ == '__main__':
