c3c66ac0eb31e6a798cd6ff8dcb7d174a73b9e1b
gan3i <gani468@gmail.com>
2020-12-25 19:32:46 +0530

    sum up binary from root to leaf

--- EPIJudge-master/epi_judge_python/sum_root_to_leaf.py
+++ EPIJudge-master/epi_judge_python/sum_root_to_leaf.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
     # TODO - you fill in here.
-    return 0
+    def sum(tree, running_sum):
+        if not tree:
+            return 0
 
+        partial_sum = running_sum << 1 | tree.data
+        if not tree.left and not tree.right:
+            return partial_sum
+
+        return sum(tree.left, partial_sum) + sum(tree.right, partial_sum)    
+
+    return sum(tree,0)
 
 if __name__ == '__main__':
     exit(
