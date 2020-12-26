9c22dfeaa3b9536b1a71f1aa8a847a59b36bc38e
gan3i <gani468@gmail.com>
2020-12-26 23:28:31 +0530

    search first key

--- EPIJudge-master/epi_judge_python/search_first_key.py
+++ EPIJudge-master/epi_judge_python/search_first_key.py
@@ -1,11 +1,35 @@
 from typing import List
+import bisect
 
 from test_framework import generic_test
 
+def search_first_of_k(A: List[int],target: int) -> int:
 
-def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if not A:
+        return -1
+    #--------------------------------
+    # index =  bisect.bisect_left(A,target)
+
+    # return index if index < len(A) and A[index] == target else -1
+    #-----------------------------------
+
+    low = 0
+    high = len(A) - 1
+
+    while low <= high:
+        mid = low + (high-low) // 2
+
+        if A[mid] == target:
+            if mid == 0 or A[mid-1] != A[mid]:
+                return mid
+            else:
+                high = mid - 1
+        elif target < A[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1 
+
+    return -1
 
 
 if __name__ == '__main__':
