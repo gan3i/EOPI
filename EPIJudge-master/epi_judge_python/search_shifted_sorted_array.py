1f1874d4bb5a2829eee3131b76e2b0d68f4fcf27
gan3i <gani468@gmail.com>
2020-12-27 07:31:39 +0530

    soreted shifted search

--- EPIJudge-master/epi_judge_python/search_shifted_sorted_array.py
+++ EPIJudge-master/epi_judge_python/search_shifted_sorted_array.py
@@ -2,10 +2,34 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def search_smallest(A: List[int]) -> int:
+
+    low = 0 
+    high = len(A) - 1
+
+    if high == 0:
+        return low
+
+    if A[low] < A[high]:
+        return low
+
+    while low < high - 1:
+
+        mid = low + (high-low) // 2
+
+        if A[mid-1] > A[mid] < A[mid+1]:
+            return mid
+        elif A[mid] < A[0]:
+            high = mid
+        else:
+            low = mid
+
+    return high
+
+
+#def search_smallest(A: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+    #return 0
 
 
 if __name__ == '__main__':
