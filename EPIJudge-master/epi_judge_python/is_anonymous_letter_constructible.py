4b0aab0ff0d53b91fc7d4f2495d5467c9c4d35e3
gan3i <gani468@gmail.com>
2020-12-27 13:16:02 +0530

    letter constructible

--- EPIJudge-master/epi_judge_python/is_anonymous_letter_constructible.py
+++ EPIJudge-master/epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,30 @@
 from test_framework import generic_test
+from typing import Dict
 
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
     # TODO - you fill in here.
+
+    char_count : Dict[chr,int] ={} 
+
+    for c in magazine_text:
+        if c in char_count:
+            char_count[c] +=1
+        else:
+            char_count[c] =1
+
+    for c in letter_text:
+        if c not in char_count:
+            return False
+        elif char_count[c] == 0:
+            return False
+        char_count[c] -= 1
+
+    # for value in char_count.values():
+    #     if value > 0:
+    #         return False
+
     return True
 
 
