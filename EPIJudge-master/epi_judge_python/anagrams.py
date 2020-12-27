adcb32ed34f6c2ad16f9648c7c4926e00d3c71c1
gan3i <gani468@gmail.com>
2020-12-27 11:13:36 +0530

    group anagrams

--- EPIJudge-master/epi_judge_python/anagrams.py
+++ EPIJudge-master/epi_judge_python/anagrams.py
@@ -1,11 +1,18 @@
 from typing import List
+import collections
+from typing import List, DefaultDict
 
 from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
     # TODO - you fill in here.
-    return []
+    anagram_groups : DefaultDict[str,List[str]] = collections.defaultdict(list) 
+
+    for s in dictionary:
+        anagram_groups[''.join(sorted(s))].append(s)
+
+    return [group for group in anagram_groups.values() if len(group) >= 2]
 
 
 if __name__ == '__main__':
