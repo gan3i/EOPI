ef3995496fcaa9b158e3492911ec807b5f1e7b6e
gan3i <gani468@gmail.com>
2020-12-27 07:52:08 +0530

    simplified search

--- EPIJudge-master/epi_judge_python/search_shifted_sorted_array.py
+++ EPIJudge-master/epi_judge_python/search_shifted_sorted_array.py
@@ -4,25 +4,17 @@ from test_framework import generic_test
 
 def search_smallest(A: List[int]) -> int:
 
-    low = 0 
+    low = 0
     high = len(A) - 1
 
-    if high == 0:
-        return low
-
-    if A[low] < A[high]:
-        return low
-
-    while low < high - 1:
+    while low < high:
 
         mid = low + (high-low) // 2
 
-        if A[mid-1] > A[mid] < A[mid+1]:
-            return mid
-        elif A[mid] < A[0]:
-            high = mid
+        if A[mid] > A[high]:
+            low  = mid +1
         else:
-            low = mid
+            high = mid
 
     return high
 
