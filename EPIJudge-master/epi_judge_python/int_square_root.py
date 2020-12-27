de9572edfcacc8158ddb9c81f43ec383cb35245a
gan3i <gani468@gmail.com>
2020-12-27 08:40:02 +0530

    interger square root

--- EPIJudge-master/epi_judge_python/int_square_root.py
+++ EPIJudge-master/epi_judge_python/int_square_root.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+def square_root( k :int) -> int:
 
-def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if k <=1:
+        return k
+    low = 1
+    high = k
+
+    while low < high -1:
+
+        mid = low + (high - low)//2
+
+        if mid ** 2 > k:
+            high = mid
+        elif mid ** 2 < k:
+            low = mid
+        else:
+            return mid
+
+    return low
+
+
 
 
 if __name__ == '__main__':
