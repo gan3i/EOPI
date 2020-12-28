0b4a7d966d032c978fe5169416ea68fa3a62c8bc
gan3i <gani468@gmail.com>
2020-12-28 09:09:41 +0530

    merge two sorted arrays (one array has extra space)

--- EPIJudge-master/epi_judge_python/two_sorted_arrays_merge.py
+++ EPIJudge-master/epi_judge_python/two_sorted_arrays_merge.py
@@ -6,7 +6,22 @@ from test_framework import generic_test
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
     # TODO - you fill in here.
-    return
+    end = m + n -1
+    m , n = m -1, n-1
+    while m >= 0 and n >= 0:
+        if A[m] < B[n]:
+            A[end] = B[n]
+            end -=1
+            n -= 1
+        else:
+            A[end] = A[m]
+            end -=1
+            m -= 1
+
+    while n >= 0:
+        A[end] = B[n]
+        end -=1
+        n -= 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
