9e6006bf643aba7d93e31bd5af1690c64ebe5fa3
gan3i <gani468@gmail.com>
2021-01-02 23:20:23 +0530

    k largest elements in bst in decreasing order

--- EPIJudge-master/epi_judge_python/k_largest_values_in_bst.py
+++ EPIJudge-master/epi_judge_python/k_largest_values_in_bst.py
@@ -6,7 +6,18 @@ from test_framework import generic_test, test_utils
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    def build_k_largest_list(root):
+        if not root:
+            return
+        if len(k_largest_elements) < k:
+            build_k_largest_list(root.right)
+            if len(k_largest_elements) < k:
+                k_largest_elements.append(root.data)
+                build_k_largest_list(root.left)
+
+    k_largest_elements: List[int] = []
+    build_k_largest_list(tree)
+    return k_largest_elements
 
 
 if __name__ == '__main__':
