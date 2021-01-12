3a64752fc4ca005de78d227c285e6a8af0f7c4c9
gan3i <gani468@gmail.com>
2021-01-12 12:54:35 +0530

    iteration solution

--- EPIJudge-master/epi_judge_python/phone_number_mnemonic.py
+++ EPIJudge-master/epi_judge_python/phone_number_mnemonic.py
@@ -1,21 +1,29 @@
+import collections
+
+from queue import LifoQueue
 from typing import List
 
 from test_framework import generic_test, test_utils
 
 
+mnemonic = collections.namedtuple("Mnemonic",('count','running_mnemonic'))
+
 mapping = ['0','1','ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
 def phone_mnemonic(phone_number: str) -> List[str]:
-    return phone_mnemonic_helper([], phone_number, 0, ['0']*len(phone_number))
+    first = mnemonic(0,'')
+    stack = LifoQueue()
+    stack.put(first)
+    result = []
 
-def phone_mnemonic_helper(result, phone_number, count, running_mnemonic):
-    if count == len(phone_number):
-        result.append(''.join(running_mnemonic))
-    else:
-        for char in mapping[int(phone_number[count])]:
-            running_mnemonic[count] = char
-            phone_mnemonic_helper(result, phone_number, count + 1, running_mnemonic)
-            # num_string.pop()
+    while not stack.empty():
+        curr = stack.get()
+        if curr.count == len(phone_number):
+            result.append(curr.running_mnemonic)
+        else:
+            for char in mapping[int(phone_number[curr.count])]:
+                stack.put(mnemonic(curr.count + 1, curr.running_mnemonic +char))
     return result
+
 
 
 if __name__ == '__main__':
