baca4c053f7243a214eff0ee1fe6e1df1a01530a
gan3i <gani468@gmail.com>
2021-01-12 12:34:09 +0530

    rescursion solution

--- EPIJudge-master/epi_judge_python/phone_number_mnemonic.py
+++ EPIJudge-master/epi_judge_python/phone_number_mnemonic.py
@@ -3,9 +3,19 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
+mapping = ['0','1','ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    return phone_mnemonic_helper([], phone_number, 0, ['0']*len(phone_number))
+
+def phone_mnemonic_helper(result, phone_number, count, running_mnemonic):
+    if count == len(phone_number):
+        result.append(''.join(running_mnemonic))
+    else:
+        for char in mapping[int(phone_number[count])]:
+            running_mnemonic[count] = char
+            phone_mnemonic_helper(result, phone_number, count + 1, running_mnemonic)
+            # num_string.pop()
+    return result
 
 
 if __name__ == '__main__':
