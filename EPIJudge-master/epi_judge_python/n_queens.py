4ebf6d683c1b09d792c851bb872e83facab9eac0
gan3i <gani468@gmail.com>
2021-01-13 20:08:11 +0530

    n queens problem

--- EPIJudge-master/epi_judge_python/n_queens.py
+++ EPIJudge-master/epi_judge_python/n_queens.py
@@ -5,7 +5,20 @@ from test_framework import generic_test
 
 def n_queens(n: int) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    def solve_n_queens(row):
+        if row == n:
+            result.append(col_placement.copy())
+            return
+        for col in range(n):
+            if all(abs(c - col) not in (0, row -  i) 
+            for i, c in enumerate(col_placement[:row])):
+                col_placement.append(col)
+                solve_n_queens(row + 1)
+                col_placement.pop()
+    result = []
+    col_placement = []
+    solve_n_queens(0)
+    return result
 
 
 def comp(a, b):
