1044a2f5a09eb2791843da002812781f77e58da2
gan3i <gani468@gmail.com>
2021-01-17 23:33:46 +0530

    what I understood

--- EPIJudge-master/epi_judge_python/n_queens.py
+++ EPIJudge-master/epi_judge_python/n_queens.py
@@ -4,14 +4,14 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
     def solve_n_queens(row):
         if row == n:
             result.append(col_placement.copy())
             return
         for col in range(n):
-            if all(abs(c - col) not in (0, row -  i) 
-            for i, c in enumerate(col_placement[:row])):
+            #x = [[abs(c - col), (0, row -  i)] for i, c in enumerate(col_placement[:row])]
+            if all(c != col and row - col != r - c and row + col != r + c for r,c in enumerate(col_placement)):
+            #if all(abs(c - col) not in (0, row -  i) for i, c in enumerate(col_placement)):
                 col_placement.append(col)
                 solve_n_queens(row + 1)
                 col_placement.pop()
@@ -20,6 +20,7 @@ def n_queens(n: int) -> List[List[int]]:
     solve_n_queens(0)
     return result
 
+# n_queens(4)
 
 def comp(a, b):
     return sorted(a) == sorted(b)
