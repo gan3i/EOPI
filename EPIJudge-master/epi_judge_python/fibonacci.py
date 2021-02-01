f836e657eee82e5d9bd23d82bc4c6043c265a6c1
gan3i <gani468@gmail.com>
2021-02-01 20:40:52 +0530

    fib dynamic programming.

--- EPIJudge-master/epi_judge_python/fibonacci.py
+++ EPIJudge-master/epi_judge_python/fibonacci.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def fibonacci(n: int) -> int:
     # TODO - you fill in here.
-    return -1
+    if n <= 1:
+        return n
+    fib_minus_one, fib_minus_two = 1, 0
+    for _ in range(1,n):
+        fib = fib_minus_one + fib_minus_two
+        fib_minus_two = fib_minus_one
+        fib_minus_one = fib
+    return fib_minus_one
 
 
 if __name__ == '__main__':
